d03692d87b15808ad0d641b82f6c2afdcfeb7cf9
Dylon Shade <dylonshade@gmail.com>
2026-05-12 17:47:12 -0500

    Server index fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 app.use('/states', require('./routes/api/states'));
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
